a15544228b30584ee418e7fa8a433297638d40fa
n <2585803+tt5@users.noreply.github.com>
2022-12-21 15:59:36 +0100

    added tailwindcss

--- shared/ui/src/lib/banner/banner.tsx
+++ shared/ui/src/lib/banner/banner.tsx
@@ -1,12 +1,10 @@
-import styles from './banner.module.css';
-
 /* eslint-disable-next-line */
 export interface BannerProps {}
 
 export function Banner(props: BannerProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Banner!</h1>
+    <div className="bg-blue-600 text-white font-bold">
+      <h1>tt5</h1>
     </div>
   );
 }
--- shared/ui/src/lib/shared-ui.tsx
+++ shared/ui/src/lib/shared-ui.tsx
@@ -1,5 +1,3 @@
-import styles from './shared-ui.module.css';
-
 /* eslint-disable-next-line */
 export interface SharedUiProps {}
 
