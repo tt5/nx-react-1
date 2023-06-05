6a9071b9c18cb5e02ee4a2f4325108346d5b7d4d
n <2585803+tt5@users.noreply.github.com>
2023-06-05 11:38:03 +0200

    end of netninja tutorial

--- about/src/lib/about.tsx
+++ about/src/lib/about.tsx
@@ -1,11 +1,20 @@
+import {useState} from "react"
+import {Navigate} from "react-router-dom"
 
 /* eslint-disable-next-line */
 export interface AboutProps {}
 
 export function About(props: AboutProps) {
+  const  [user, setUser] = useState('john')
+
+  if (user=="") {
+    return <Navigate to="/" replace={true} />
+  }
   return (
     <>
+      <h2>About</h2>
       About
+      <button onClick={() => setUser("")}>Logout</button>
     </>
   );
 }
